f123792eb2df240adf69ade7d7037357f42b5519
Gaurav Deshmukh <gauravdeshmukh@inorexstudio.com>
2017-06-13 13:45:09 -0400

    initial commit

---
+++ src/main/java/com/gauravdeshmukh/springup/SpringupApplication.java
@@ -0,0 +1,12 @@
+package com.gauravdeshmukh.springup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringupApplication.class, args);
+	}
+}
